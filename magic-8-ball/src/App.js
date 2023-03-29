9dc806f466d784fc6763de49de1477dae1522803
Chantal Guiheen <chantal@chantals-air.lan>
2023-03-29 15:34:12 +0100

    JS game logic

--- magic-8-ball/src/App.js
+++ magic-8-ball/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+const [userInput, setUserInput] = useState('');
+const [randomIndex, setRandomIndex] = useState('');
+const [error, setError] = useState('');
+
+const handleChange = (event) => {
+  event.preventDefault();
+  console.log(event.target.value);
+  setUserInput(event.target.value);
+}
+
+const handleClick = () => {
+
+  if(userInput)
+  {
+    setError('')
+  setRandomIndex(Math.round(Math.random() * 20));
+  setUserInput('');
+  }
+  else{
+    setError('Ask a Question First...')
+  }
+  console.log(randomIndex);
+}
+
+const possibleAnswers = [
+  'It Is Certain',
+  'It Is Decidedly So',
+  'Without A Doubt!',
+  'Yes, Definitely',
+  'You May Rely On It',
+  'As I See It, Yes!',
+  'Outlook Good!',
+  'Yes!',
+  'Signs Point To Yes',
+  'Reply Hazy. Try Again',
+  'Ask Again Later...',
+  'Better Not Tell You Now',
+  'Cannot Predict Now',
+  'Concentrate and Ask Again',
+  'Don\'t Count On It',
+  'My Reply Is No!',
+  'My Sources Say No',
+  'Most Likely',
+  'Outlook Not So Good',
+  'Very Doubtful!'
+];
+const answer = possibleAnswers[randomIndex];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic 8-Ball</h1>
+      <p className="info">Type a question and click me for an answer...</p>
+      <input type="text" className="question" value={userInput} onChange={handleChange} />
+      <div className="eight-ball">
+        <div className="content" onClick={handleClick}>
+        {error ? <p className="error">{error}</p> :
+          answer ? <p className="answer">{answer}</p> : <p className="eight">8</p>}
+          
+        </div>
+      </div>
     </div>
   );
 }
